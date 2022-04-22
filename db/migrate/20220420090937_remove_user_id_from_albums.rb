a0d3ebc99a4680631696260d90c1e69648a51df7
Gaurav Gupta <gauravgupta19911@gmail.com>
2022-04-22 10:20:17 +0530

    hello

--- db/migrate/20220420090937_remove_user_id_from_albums.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdFromAlbums < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :albums, :user_id, :string
-  end
-end
--- db/migrate/20220420091046_remove_user_id_id_from_albums.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdIdFromAlbums < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :albums, :user_id_id, :string
-  end
-end
